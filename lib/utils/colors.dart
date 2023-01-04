b2c007a960d667acc13a286499b2d5f161cf5e42
HinakIvan <ginakvania@gmail.com>
2023-01-04 17:10:01 +0100

    my first commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static final Color mainColor = const Color(0xFF89dad0);
+  static final Color mainBlackColor = const Color(0xFF332d2b);
+}
